0f33b6b0b05b20a00f6317fcd091966e70e07efa
Reinder Nijhoff <reinder@infi.nl>
2023-11-14 14:24:42 +0100

    Update Panorama.js

--- example/src/examples/Panorama.js
+++ example/src/examples/Panorama.js
@@ -4,8 +4,8 @@ import ImageLoader from '../utils/ImageLoader';
 export default class Panorama {
   constructor(container) {
     ImageLoader.loadImages(['panorama_2.jpg']).then((images) => {
-      this.renderer = new PanoramaRenderer(container, images[0]);
-      this.renderer.play();
+      this.renderer = new PanoramaRenderer(container, images[0], {loop: true});
+      // this.renderer.play();
     });
   }
 }
