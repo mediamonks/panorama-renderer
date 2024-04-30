690eaa7fd968e9df655d55b434bad15ba9a39664
Reinder Nijhoff <reinder@infi.nl>
2024-04-30 09:49:00 +0200

    example fixes

--- example/src/examples/Panorama.js
+++ example/src/examples/Panorama.js
@@ -1,11 +1,11 @@
-import {PanoramaRenderer} from '../../../dist/';
+import {PanoramaRenderer} from '@mediamonks/panorama-renderer';
 import ImageLoader from '../utils/ImageLoader';
 
 export default class Panorama {
-  constructor(container) {
-    ImageLoader.loadImages(['panorama_2.jpg']).then((images) => {
-      this.renderer = new PanoramaRenderer(container, images[0], {loop: true});
-      // this.renderer.play();
-    });
-  }
+    constructor(container) {
+        ImageLoader.loadImages(['panorama_2.jpg']).then((images) => {
+            this.renderer = new PanoramaRenderer(container, images[0], {loop: true});
+            // this.renderer.play();
+        });
+    }
 }
--- example/src/examples/PanoramaAdvanced.js
+++ example/src/examples/PanoramaAdvanced.js
@@ -1,5 +1,4 @@
-import {PanoramaRenderer} from '../../../dist/';
-
+import {PanoramaRenderer} from '@mediamonks/panorama-renderer';
 import glitch from '../shader/glitch.glsl';
 import panoramaMix from '../shader/panoramaMix.glsl';
 import ImageLoader from "../utils/ImageLoader";
@@ -7,52 +6,52 @@ import {ImageEffectRenderer} from "@mediamonks/image-effect-renderer";
 import {quatIdentity, quatRotateY} from "../../../dist/";
 
 class AutoRotationController {
-  constructor() {
-    this.rotation = 0;
-  }
+    constructor() {
+        this.rotation = 0;
+    }
 
-  init(renderer, options) {
-  }
+    init(renderer, options) {
+    }
 
-  update(dt, rotation) {
-    this.rotation += .2 * dt;
-    return quatRotateY(quatIdentity(), this.rotation);
-  }
+    update(dt, rotation) {
+        this.rotation += .2 * dt;
+        return quatRotateY(quatIdentity(), this.rotation);
+    }
 }
 
 export default class PanoramaAdvanced {
-  constructor(container) {
-    this.container = container;
-    this.time = 0;
+    constructor(container) {
+        this.container = container;
+        this.time = 0;
 
-    ImageLoader.loadImages(['panorama_1.jpg', 'panorama_2.jpg']).then((images) => {
-      this.renderer = ImageEffectRenderer.createTemporary(this.container, glitch, {useSharedContext: false});
+        ImageLoader.loadImages(['panorama_1.jpg', 'panorama_2.jpg']).then((images) => {
+            this.renderer = ImageEffectRenderer.createTemporary(this.container, glitch, {useSharedContext: false});
 
-      this.renderer.createBuffer(0, panoramaMix);
-      this.renderer.buffers[0].setImage(0, images[0], {
-        clampX: false,
-        flipY: true,
-        useMipmap: true
-      });
-      this.renderer.buffers[0].setImage(1, images[1], {
-        clampX: false,
-        flipY: true,
-        useMipmap: true
-      });
+            this.renderer.createBuffer(0, panoramaMix);
+            this.renderer.buffers[0].setImage(0, images[0], {
+                clampX: false,
+                flipY: true,
+                useMipmap: true
+            });
+            this.renderer.buffers[0].setImage(1, images[1], {
+                clampX: false,
+                flipY: true,
+                useMipmap: true
+            });
 
-      this.renderer.setImage(0, this.renderer.buffers[0]);
+            this.renderer.setImage(0, this.renderer.buffers[0]);
 
-      this.panorama = new PanoramaRenderer(this.container, null, {
-        // renderer: this.renderer,
-        fov: 90,
-        rotationController: new AutoRotationController(),
-        controlledRendererInstance: this.renderer.buffers[0],
-      });
-      this.panorama.play();
-      this.panorama.tick((dt) => {
-        this.time += dt;
-        this.renderer.buffers[0].setUniformFloat('uMix', Math.abs(((this.time / 10) % 1) - .5) * 2);
-      });
-    });
-  }
+            this.panorama = new PanoramaRenderer(this.container, null, {
+                // renderer: this.renderer,
+                fov: 90,
+                rotationController: new AutoRotationController(),
+                controlledRendererInstance: this.renderer.buffers[0],
+            });
+            this.panorama.play();
+            this.panorama.tick((dt) => {
+                this.time += dt;
+                this.renderer.buffers[0].setUniformFloat('uMix', Math.abs(((this.time / 10) % 1) - .5) * 2);
+            });
+        });
+    }
 }
